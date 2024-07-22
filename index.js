d445d4bafd902d702ec1435cf670ff38b521c1f7
JuliusBTI <j.baksinskas1988@gmail.com>
2024-07-22 11:06:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import { Car } from "./js/car";
+
+const masina = new Car('audi', "TT", 'black', 60, 9, 0);
+
+console.log(masina)
+
---
+++ js/Car.js
@@ -0,0 +1,16 @@
+export class Car {
+    constructor(pavadinimas, modelis, spalva) {
+        this.brand = pavadinimas;
+        this.model = modelis;
+        this.color = spalva ;
+        this.capacity = talpa;
+        this.consumption = sanaudos;
+        this.speed = greitis;
+        this.engineOn = false;
+    }
+
+}
+
+
+
+
